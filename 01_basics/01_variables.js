7815784db62a8cbea7306a1da31442fd40e46b1f
Shubham Anand <144546060+sshubham23@users.noreply.github.com>
2024-07-03 19:16:24 +0000

    variables learn

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail = "ssa@amazon.com"
+var accountPass = "098"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 123 //Not Allowed as it's of const type
+// console.log(accountId)
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
