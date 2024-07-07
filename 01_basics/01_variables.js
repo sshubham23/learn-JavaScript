18b0f5424aac1508bdb28fa2bd6bf3511efaf8c8
Shubham Anand <144546060+sshubham23@users.noreply.github.com>
2024-07-07 17:42:08 +0000

    updated

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -7,4 +7,9 @@ let accountState
 // accountId = 123 //Not Allowed as it's of const type
 // console.log(accountId)
 
+/* Scope:
+var is function-scoped. If you declare a variable with var inside a function, it is accessible anywhere within that function.
+let is block-scoped. If you declare a variable with let inside a block (e.g., within curly braces {}), it is only accessible within that block.
+*/
+
 console.table([accountId, accountEmail, accountPass, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Hello Sshubham")  //we are using Nodejs not browser
+
+
+// Code readability must be High
+console.log("Hello Sshubham");      
+
+console.log("Why write Sshubham")   // most of the case semicolon(;) can be ignored in js
+
+let name = "Sshubham" //String
+let age = 21            //int
+let isLoggedIn = false  //Boolean
+
+//1. number => 2^53
+//2. bigint
+//3. string => ""
+//4. boolean -> true/false
+//5. null=> standalone value
+//6. undefined => means value not assigned yet
+//7. symbol => uniqueness
+//8. object
+
+console.log(typeof "Sshubham")
+
+console.log(typeof undefined)   //undefined
+//BUT
+console.log(typeof null)    //object
